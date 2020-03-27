864943b9a94d80930d3c8a9067632d14ca14b2d5
Neyzoter <sonechaochao@gmail.com>
2020-03-27 20:28:34 +0800

    update

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,7 @@
 //请求配置
 //  export const requestIp = 'http://garden.longjing.terabits-wx.cn:4000'; //移动服务器
 //export const requestIp = "http://122.152.249.37:4000"; //测试服务器
-export const requestIp = "http://172.16.1.53:9092";
+export const requestIp = "http://172.17.0.1:9092";
 // export const requestIp = "http://localhost:9099";
 
 export const downUrl= requestIp//下载报告地址
