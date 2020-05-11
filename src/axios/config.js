875c4de66130e81ec8519eb543dab228d89ce858
Neyzoter <sonechaochao@gmail.com>
2020-05-11 09:02:17 +0800

    update ip

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,5 +1,5 @@
 //请求配置
-// export const requestIp = "http://ra.sorl.com.cn:9097";
-export const requestIp = "http://localhost:9099";
+export const requestIp = "http://ra.sorl.com.cn:9097";
+// export const requestIp = "http://localhost:9099";
 
 export const downUrl= requestIp//下载报告地址
